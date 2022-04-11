42c831e7aa29b6b4858da0c83ca6140343d59c83
lwagenciaweb <lwagenciaweb@gmail.com>
2022-04-11 11:10:14 -0300

    Inicialização do projeto

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright 11/04/2022 Google LLC
+ * 
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at 
+ * https://opensource.org/licenses/MIT.
+ * @author Lucas O. Silva <? PHP Developer ?>
+ * @instagram @_lucaslw
+ 
+ */ module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 11/04/2022 Google LLC
+ * 
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at 
+ * https://opensource.org/licenses/MIT.
+ * @author Lucas O. Silva <? PHP Developer ?>
+ * @instagram @_lucaslw
+ 
+ */
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
